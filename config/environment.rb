8372272dda20553dc80740df098051798045a11d
Alex Treglia <my2cent010@gmail.com>
2014-03-31 21:37:52 -0400

    Project

---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+# Load the Rails application.
+require File.expand_path('../application', __FILE__)
+
+# Initialize the Rails application.
+My2cents::Application.initialize!
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+My2cents::Application.config.session_store :cookie_store, key: '_my2cents_session'
